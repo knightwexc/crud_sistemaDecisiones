3c4f277697edf86ef35c90bf1d5f8ebb7bf9ac8d
Jonathan Calderón <jonathan.calesq@gmail.com>
2022-11-26 19:53:51 -0700

    Se creo tabla e integro datos con servicios

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpleadosService } from '../service/empleados.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  styleUrls: ['./lista.component.css'],
 })
 export class ListaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private empleadosService: EmpleadosService) {}
 
   ngOnInit(): void {
+    this.listarEmpleados();
   }
 
+  displayedColumns: string[] = [
+    'id',
+    'nombre',
+    'apellidos',
+    'domicilio',
+    'puesto',
+    'sucursal',
+    'sueldo',
+    'prioridad',
+  ];
+  arrayService: [] = [];
+  listarEmpleados() {
+    this.empleadosService.getEmpleados().subscribe(
+      (res) => {
+        this.arrayService = res;
+        console.log(this.arrayService);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
