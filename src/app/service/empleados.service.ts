fe074fc5352079f1797ae506056f722001ac8812
Jonathan Calderón <jonathan.calesq@gmail.com>
2022-11-30 05:56:36 -0700

    agregado editar empleado al serv y empezando ajustes para productos

--- src/app/service/empleados.service.ts
+++ src/app/service/empleados.service.ts
@@ -19,16 +19,19 @@ export class EmpleadosService {
 
   guardarEmpleado(empleado: Empleado): Observable<any> {
     return this.http.post(this.url + '/empleado', empleado);
-    console.log('empleado guardado');
+  }
+
+  editarEmpleado(id: number, empleado: Empleado): Observable<any> {
+    return this.http.put(this.url + '/empleado/' + id, empleado);
   }
 
   borrarEmpleado(id: number): Observable<any> {
     return this.http.delete(this.url + '/empleado/' + id);
   }
-
-  // editarEmpleado(empleado: Empleado): Observable<any> {
-  //   return this.http.put(this.url + '/empleado/' + id);
-  // }
+  //SERVICIO PARA EMPLEADOS
+  getProductos(): Observable<any> {
+    return this.http.get(this.url + '/producto');
+  }
 }
 
 export interface Empleado {
